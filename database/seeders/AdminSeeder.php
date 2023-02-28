b27248846e82a1f80525d0257878a73179c7caee
shahariarikbal <shahariar.ikbal86@gmail.com>
2023-02-28 20:33:34 +0000

    admin dashbaord init

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User();
+        $admin->name = 'Admin';
+        $admin->email = 'admin@admin.com';
+        $admin->password = bcrypt(12345678);
+        $admin->save();
+    }
+}
