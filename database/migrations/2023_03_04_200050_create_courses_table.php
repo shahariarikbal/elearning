9b119a8385e6b0456ba35d0a91d85e38a5a3c450
Muntasir-Pranto <fahimmuntasir43@gmail.com>
2023-03-05 02:17:49 +0600

    Model and Migration are created for courses

---
+++ database/migrations/2023_03_04_200050_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('short_description');
+            $table->longText('long_description');
+            $table->string('type');
+            $table->string('real_price');
+            $table->string('discount_price')->nullable();
+            $table->string('lesson');
+            $table->boolean('status')->default(true);
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
