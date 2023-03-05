f71ad1e6e6b1e842a89cb4e169e18ce2b28ac9ac
shahariarikbal <shahariar.ikbal86@gmail.com>
2023-03-05 20:05:36 +0000

    fix:service added

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ServiceRequest;
+use App\Models\Service;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,11 +12,34 @@ class ServiceController extends Controller
 {
     public function services()
     {
-        return Inertia::render('Backend/Service');
+        $services = Service::orderBy('created_at', 'desc')->get();
+        return Inertia::render('Backend/Service', ['services' => $services]);
     }
 
     public function addService()
     {
         return Inertia::render('Backend/ServiceAdd');
+    }
+
+    public function storeService(ServiceRequest $request)
+    {
+        $imageName = time() . '.' . $request->image->extension();
+        $imagePath = $request->image->move('service/', $imageName);
+        Service::create([
+            'title' => $request->title,
+            'slug' => str_replace(' ', '-', strtolower($request->title)),
+            'image' => $imageName,
+            'description' => $request->description
+        ]);
+        sleep(1);
+        return redirect()->route('services')->with('success', 'Service has been created');
+    }
+
+    public function deleteService($id)
+    {
+        $service = Service::find($id);
+        $service->delete();
+        sleep(1);
+        return redirect()->route('services')->with('success', 'Service has been deleted');
     }
 }
