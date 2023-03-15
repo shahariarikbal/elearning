a2ab7eb21269a3f74a7fd7a11e3366c16b8f011f
shahariarikbal <shahariar.ikbal86@gmail.com>
2023-03-15 20:43:36 +0000

    feat:course enroll

--- app/Http/Controllers/Frontend/AuthenticateController.php
+++ app/Http/Controllers/Frontend/AuthenticateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Enroll;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -23,7 +24,7 @@ class AuthenticateController extends Controller
 
     public function userStore(Request $request)
     {
-        dd($request->course_id);
+        //dd($request->course_id);
         $this->validate($request, [
             'first_name' => 'required|string',
             'last_name' => 'required|string',
@@ -36,16 +37,22 @@ class AuthenticateController extends Controller
         $imageName = time() . '.' . $request->avatar->extension();
         $image = $request->avatar->move('/avatar/',$imageName);
 
-        $data = [
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'avatar' => $imageName,
-            'password' => bcrypt($request->phone),
-        ];
-        User::create($data);
+        $user = new User();
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->password = bcrypt($request->phone);
+        $user->avatar = $imageName;
+        $user->save();
+        //After enroll insert enroll table data
+        if ($user->save()){
+            $enroll = new Enroll();
+            $enroll->user_id = $user->id;
+            $enroll->course_id = $request->course_id;
+            $enroll->save();
+        }
         sleep(1);
-        return redirect('/user/register')->with('message', 'Thank you for course enroll , Your Registration has been successfully done.');
+        return redirect()->back()->with('message', 'Thank you for course enroll , Your Registration has been successfully done.');
     }
 }
