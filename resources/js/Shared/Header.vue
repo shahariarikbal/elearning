4d1f2a99b662cc07b805bad75bc0fe1ac19d797a
Saidul Islam <saidulislamjihad20@gmail.com>
2023-03-30 14:40:11 +0600

    fix:Responsive header

--- resources/js/Shared/Header.vue
+++ resources/js/Shared/Header.vue
@@ -1,51 +1,53 @@
 <template>
     <div>
-        <header class="header-section">
-            <nav class="nav-items-wrapper">
-                <NavLink href="/" class="navbar-brand">
-                    <img class="brand" :src="'/setting/' + $page.props.setting.allSetting.logo" alt="Logo">
-                </NavLink>
-                <div class="nav-toggle-btn" onclick="myFunction()">
-                    <div class="btn-inner"></div>
-                </div>
-                <ul class="nav-list" id="navList">
-                    <li class="nav-item">
-                        <NavLink href="/" class="nav-item-link active">
-                            হোম
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink href="/courses" class="nav-item-link">
-                            কোর্স সমূহ
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink href="/blog" class="nav-item-link">
-                            ব্লগ
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink href="/team/members" class="nav-item-link">
-                            টীম
-                        </NavLink>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-item-link">
-                            লাইভ ব্যাচ
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-item-link">
-                            অফার
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <NavLink href="/user/login" class="nav-item-link login-logout">
-                            লগ ইন / সাইন আপ
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+        <header class="header-section" id="header">
+            <div class="container">
+                <nav class="nav-items-wrapper">
+                    <NavLink href="/" class="navbar-brand">
+                        <img class="brand" :src="'/setting/' + $page.props.setting.allSetting.logo" alt="Logo">
+                    </NavLink>
+                    <div class="nav-toggle-btn">
+                        <div class="btn-inner"></div>
+                    </div>
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <NavLink href="/" class="nav-item-link active">
+                                হোম
+                            </NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink href="/courses" class="nav-item-link">
+                                কোর্স সমূহ
+                            </NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink href="/blog" class="nav-item-link">
+                                ব্লগ
+                            </NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink href="/team/members" class="nav-item-link">
+                                টীম
+                            </NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-item-link">
+                                লাইভ ব্যাচ
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-item-link">
+                                অফার
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink href="/user/login" class="nav-item-link login-logout">
+                                লগ ইন / সাইন আপ
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>            
         </header>
     </div>
 </template>
@@ -59,8 +61,9 @@ export default {
         NavLink
     }
 }
+
 </script>
 
 <style scoped>
-
+    
 </style>
